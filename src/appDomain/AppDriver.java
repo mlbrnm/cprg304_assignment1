1bbd501704941298d068059ab7012ea609245598
Melbourne Marsden <114309689+mlbrnm@users.noreply.github.com>
2025-06-02 14:49:09 -0600

    Add methods to compare base area and volume. Add function to load the shapes from the files into arrays.

--- src/appDomain/AppDriver.java
+++ src/appDomain/AppDriver.java
@@ -1,25 +1,87 @@
 package appDomain;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import shapes.*;
+
 public class AppDriver
 {
 
-	public static void main( String[] args )
-	{
-		// TODO Auto-generated method stub
+    public static void main( String[] args )
+    {
+        Shape[][] shapesArrays = loadShapesFromFiles();
+        for (int i = 0; i < shapesArrays.length; i++) {
+            System.out.println("Loaded " + shapesArrays[i].length + " shapes from file " + (i+1));
+        }
+    }
 
-		// refer to demo001 BasicFileIO.java for a simple example on how to
-		// read data from a text file
+    public static Shape[][] loadShapesFromFiles() {
+        File resFolder = new File("res");
+        File[] files = resFolder.listFiles((dir, name) -> name.matches("shapes\\d+\\.txt"));
+        if (files == null) {
+            System.out.println("No shape files found in res folder.");
+            return new Shape[0][0];
+        }
 
-		// refer to demo01 Test.java for an example on how to parse command
-		// line arguments and benchmarking tests
+        Shape[][] shapesArrays = new Shape[files.length][];
+        for (int f = 0; f < files.length; f++) {
+            File file = files[f];
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                if (line == null) {
+                    shapesArrays[f] = new Shape[0];
+                    continue;
+                }
+                int numShapes = Integer.parseInt(line.trim());
+                Shape[] shapes = new Shape[numShapes];
 
-		// refer to demo02 Student.java for comparable implementation, and
-		// NameCompare.java or GradeCompare for comparator implementations
+                for (int i = 0; i < numShapes; i++) {
+                    line = br.readLine();
+                    if (line == null) break;
+                    String[] parts = line.split("\\s+");
+                    if (parts.length != 3) continue;
 
-		// refer to demo02 KittySort.java on how to use a custom sorting
-		// algorithm on a list of comparables to sort using either the
-		// natural order (comparable) or other orders (comparators)
+                    String shapeType = parts[0];
+                    double height = Double.parseDouble(parts[1]);
+                    double param = Double.parseDouble(parts[2]);
 
-	}
+                    Shape shape = createShape(shapeType, height, param);
+                    if (shape != null) {
+                        shapes[i] = shape;
+                    }
+                }
+                shapesArrays[f] = shapes;
+            } catch (IOException e) {
+                System.out.println("Error reading file: " + file.getName());
+                e.printStackTrace();
+                shapesArrays[f] = new Shape[0];
+            }
+        }
 
+        return shapesArrays;
+    }
+
+    private static Shape createShape(String shapeType, double height, double param) {
+        switch (shapeType) {
+            case "OctagonalPrism":
+                return new OctagonalPrism(param, height);
+            case "Pyramid":
+                return new Pyramid(param, height);
+            case "TriangularPrism":
+                return new TriangularPrism(param, height);
+            case "Cylinder":
+                return new Cylinder(param, height);
+            case "Cone":
+                return new Cone(param, height);
+            case "PentagonalPrism":
+                return new PentagonalPrism(param, height);
+            case "SquarePrism":
+                return new SquarePrism(param, height);
+            default:
+                System.out.println("Unknown shape type: " + shapeType);
+                return null;
+        }
+    }
 }
--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -20,7 +20,29 @@ public abstract class Shape implements Comparable<Shape>{
             return 0;
         }
     }
-    
+
+
+    public int compareBaseArea(Shape other) {
+        if (this.calcBaseArea() > other.calcBaseArea()) {
+            return -1;
+        } else if (this.calcBaseArea() < other.calcBaseArea()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+
+    public int compareVolume(Shape other) {
+        if (this.calcVolume() > other.calcVolume()) {
+            return -1;
+        } else if (this.calcVolume() < other.calcVolume()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
     public Shape(double height) {
         this.height = height;
     }
